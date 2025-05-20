11bb27d5a7f47b96acba7c7b710096b4966fd6e3
yujunggg <a90134627@gmail.com>
2025-05-21 08:58:03 +0900

    feat: create signup button

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,13 +1,23 @@
-import { StartHubButton } from '../../components/common/StartHubButton';
 import * as S from './style';
+import { StartHubButton } from '../../components/common/StartHubButton';
+import { StartHubColors } from '../../Design/color/StartHubColors';
+import { StartHubFont } from '../../Design/text/StartHubFont';
 
 const SignUp = () => {
   return (
-    <div>
+    <S.GlobalStyle>
       <S.Container>
-        <StartHubButton text = "테스트" backgroundColor='tomato' onClick={() => console.log('click')}></StartHubButton>
+        <StartHubButton
+          text="회원가입"
+          width={320}
+          height={50}
+          backgroundColor={StartHubColors.Primary}
+          typography={StartHubFont.Pretendard.Body1.Medium}
+          onClick={() => console.log("click!")}
+          textTheme={StartHubColors.White1}
+        />
       </S.Container>
-    </div>
+    </S.GlobalStyle>
   )
 }
 
--- src/pages/SignUp/style.ts
+++ src/pages/SignUp/style.ts
@@ -2,11 +2,16 @@ import styled from 'styled-components';
 // import { StartHubColors } from "../../Design/color/StartHubColors";
 // import { StartHubFont } from "../../Design/text/StartHubFont";
 
+export const GlobalStyle = styled.div`
+    background-color: #f5f5f5; // 원하는 배경색으로 변경
+    margin: 0;
+    padding: 0;
+`
+
 export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
+    width: 490px;
+    height: 715px;
+    background-color: white;
     align-items: center;
-    justify-content: center;
-    height: 100vh;
-    background-color:rgb(159, 82, 82);
 `;
+
