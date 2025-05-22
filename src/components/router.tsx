82cb716ca5dd442472f277b932d5f1803a91b852
서진교 <oygnijoes0209@gmail.com>
2025-05-22 17:44:49 +0900

    feat: add router #3

--- src/components/router.tsx
+++ src/components/router.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
+import SignInPage from "../pages/SignIn";
 
 
 const Router = () => {
   return(
     <BrowserRouter>
       <Routes>
-        <Route></Route>
+        <Route path="/sign-in" element={<SignInPage/>}/>
       </Routes>
     </BrowserRouter>
   )
