d5f6e356288ab4c99838de5e2401462e072d9e4e
서진교 <oygnijoes0209@gmail.com>
2025-05-22 20:38:12 +0900

    feat: add signin component

---
+++ src/components/SignInBox/style.ts
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+import { StartHubColors } from "../../Design/color/StartHubColors";
+import { StartHubFont } from "../../Design/text/StartHubFont";
+
+export const StartHubButton = styled.div`
+  color: ${StartHubColors.Primary};
+  ${StartHubFont.Pretendard.Headlines1}
+`;
+
+export const SignInNormalText = styled.p`
+  justify-content: center;
+  color: ${StartHubColors.Black1};
+  ${StartHubFont.Pretendard.Body1}
+  span {
+    color: ${StartHubColors.Primary};
+  }
+`;
+
+export const SignInLogo = styled.div`
+  width: 135px;
+  height: 55px;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+`;
+
+export const SignInLogoField = styled.div`
+  justify-content: center;
+  margin-bottom: 34px;
+  text-align : center;
+`;
+
+export const SignInOptions = styled.div`
+  width: 320px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 15px;
+  p {
+    color: ${StartHubColors.Gray2};
+    ${StartHubFont.Pretendard.Caption1.Medium}
+  }
+`;
+
+export const SignBoxContainer = styled.div`
+  position: absolute;
+  align-items: center;
+`;
+
+export const AutoSignInField = styled.div`
+  display: flex;
+  align-items: center;
+  p{
+    color: ${StartHubColors.Gray2};
+    ${StartHubFont.Pretendard.Caption1.Medium}
+    margin-left: 4px;
+  }
+`;
