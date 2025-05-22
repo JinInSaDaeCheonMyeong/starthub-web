d13b3116a1c48b96d628604eb3dc421ccb4fad8c
서진교 <oygnijoes0209@gmail.com>
2025-05-22 20:38:41 +0900

    feat: add signin page

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,19 @@
+import "./style";
+import SignIn from "../../components/SignInBox";
+import { LeftBoxField, RightBoxField } from "./style";
+import { ReactComponent as SignInImage } from "../../assets/images/signInImage.svg";
+
+const SignInPage = () => {
+  return (
+    <div className="signin" style={{ display: "flex", height: "100vh" }}>
+      <LeftBoxField>
+        <SignInImage style={{ width: "601px", height: "724px" }} />
+      </LeftBoxField>
+      <RightBoxField>
+        <SignIn />
+      </RightBoxField>
+    </div>
+  );
+};
+
+export default SignInPage;
