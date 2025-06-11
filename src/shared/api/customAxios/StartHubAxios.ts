64f4088fd5f4ee0acf62ce00f1e17d18014eb0c8
서진교 <oygnijoes0209@gmail.com>
2025-06-11 19:35:36 +0900

    feat: custom axios

---
+++ src/shared/api/customAxios/StartHubAxios.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+export const instance = axios.create({
+  baseURL: process.env.VITE_API_BASE_URL,
+  withCredentials: true,
+  timeout: 10000
+});
+
+instance.interceptors.request.use((config) => {
+  const token = Cookies.get('access_token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+instance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      Cookies.remove('access_token');
+      window.location.href = '/sign-in';
+    }
+    return Promise.reject(error);
+  }
+);
