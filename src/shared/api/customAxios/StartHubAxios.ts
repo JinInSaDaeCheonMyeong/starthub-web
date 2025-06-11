af12b8ad45e36a4a6e2fe71422c1980f91e48bba
서진교 <oygnijoes0209@gmail.com>
2025-06-11 21:32:14 +0900

    fix: change baseURL to import.meta.env

--- src/shared/api/customAxios/StartHubAxios.ts
+++ src/shared/api/customAxios/StartHubAxios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const StartHubAxios = axios.create({
-  baseURL: process.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
   timeout: 10000
 });
