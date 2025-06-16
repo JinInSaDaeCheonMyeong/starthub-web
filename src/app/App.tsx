6395f2f87346917bfac7bee9e8e4186c2b512d32
서진교 <oygnijoes0209@gmail.com>
2025-06-16 12:08:58 +0900

    feat: setup react-query provider #3

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,17 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Router from "@/app/router";
 import { GlobalStyle } from "@/shared/style/webfont.style";
 import GlobalToastContainer from "@/shared/ui/Toast";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyle />
       <Router />
       <GlobalToastContainer />
-    </>
+    </QueryClientProvider>
   );
 }
 
