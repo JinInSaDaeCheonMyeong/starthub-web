dca3d0319d73f60a6c52bed50884bf470a14688e
서진교 <oygnijoes0209@gmail.com>
2025-06-16 12:26:34 +0900

    refactor: add oauthState API and rename OAuth Requests #3

--- src/entities/user/api/user.ts
+++ src/entities/user/api/user.ts
@@ -1,19 +1,22 @@
 import StartHubAxios from '../../../shared/api/customAxios/StartHubAxios'
-import { AuthResponse, OAuthRequest, AuthRequest, OAuthResponse } from '../model/types'
+import { AuthResponse, GoogleOAuthRequest, AuthRequest, OAuthResponse, OAuthStateResponse, StateRequest, NaverOAuthRequest, AppleOAuthRequest } from '../model/types'
 
 
 export const userApi = {
   signIn: (data: AuthRequest): Promise<AuthResponse> =>
     StartHubAxios.post('/user/sign-in', data).then(res => res.data),
 
-  oauthGoogle: (data: OAuthRequest): Promise<OAuthResponse> =>
+  oauthGoogle: (data: GoogleOAuthRequest): Promise<OAuthResponse> =>
     StartHubAxios.get('/oauth/google', { params: data }).then(res => res.data),
 
-  oauthNaver: (data: OAuthRequest): Promise<OAuthResponse> =>
+  oauthNaver: (data: NaverOAuthRequest): Promise<OAuthResponse> =>
     StartHubAxios.get('/oauth/naver', { params: data }).then(res => res.data),
 
-  oauthApple: (data: OAuthRequest): Promise<OAuthResponse> =>
+  oauthApple: (data: AppleOAuthRequest): Promise<OAuthResponse> =>
     StartHubAxios.post('/oauth/apple', data).then(res => res.data),
+
+  oauthState: (data: StateRequest): Promise<OAuthStateResponse> =>
+    StartHubAxios.get('/oauth/state', { params: data }).then(res => res.data),
 
   userProfile: () =>
     StartHubAxios.get('/user/profile').then(res => res.data)
