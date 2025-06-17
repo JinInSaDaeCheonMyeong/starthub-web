fe4e7d7c2b61648673b5bd7b957f9b169076c015
서진교 <oygnijoes0209@gmail.com>
2025-06-17 09:14:29 +0900

    feat: add toast message #34

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Router from "@/app/router";
 import { GlobalStyle } from "@/shared/style/webfont.style";
+import GlobalToastContainer from "@/shared/ui/Toast";
 
 const queryClient = new QueryClient();
 
@@ -9,8 +10,8 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <GlobalStyle />
       <Router />
+      <GlobalToastContainer />
     </QueryClientProvider>
-    
   );
 }
 
