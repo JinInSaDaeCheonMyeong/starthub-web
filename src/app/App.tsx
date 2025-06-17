eb019295bcb1e93c6b091c60b48b3dd6e75e975e
서진교 <oygnijoes0209@gmail.com>
2025-06-17 09:14:29 +0900

    feat: add toast message #34

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,13 @@
 import Router from "@/app/router";
 import { GlobalStyle } from "@/shared/style/webfont.style";
+import GlobalToastContainer from "@/shared/ui/Toast";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Router />
+      <GlobalToastContainer />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './app/index.css'
 import App from './app/App'
+import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
