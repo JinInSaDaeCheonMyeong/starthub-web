9f5f0eb9ee68ca828f79e516ce5d8138732efa2c
yujunggg <a90134627@gmail.com>
2025-06-19 14:07:55 +0900

    chore: use StartHubColor #2

--- src/pages/SignUp/style.ts
+++ src/pages/SignUp/style.ts
@@ -23,7 +23,7 @@ export const LoginForm = styled.form`
   gap: 10px;
   display: flex;
   flex-direction: column;
-  background-color: white;
+  background-color: ${StartHubColors.White1};
   padding: 40px 80px;
   width: 100%;
 
