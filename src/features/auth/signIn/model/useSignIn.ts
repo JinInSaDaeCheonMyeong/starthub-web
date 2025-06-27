d8ce0ce80f57ce0c763d94ed61d7d41d77dcadac
서진교 <oygnijoes0209@gmail.com>
2025-06-28 01:18:58 +0900

    refactor: use toast #3

--- src/features/auth/signIn/model/useSignIn.ts
+++ src/features/auth/signIn/model/useSignIn.ts
@@ -2,6 +2,7 @@ import { useMutation } from '@tanstack/react-query';
 import { userApi } from '@/entities/user/api/user';
 import { cookieUtils } from '@/shared/lib/utils/cookieUtils';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 export const useSignIn = () => {
   const navigate = useNavigate();
@@ -15,10 +16,12 @@ export const useSignIn = () => {
     mutationFn: userApi.signIn,
     onSuccess: (response) => {
       cookieUtils.setAccessToken(response.data.access);
+      toast.success('로그인에 성공했습니다.');
       navigate('/main');
     },
-    onError: () => {
-      alert('로그인에 실패했습니다. 이메일과 비밀번호를 확인해주세요.');
+    onError: (error) => {
+      console.log(error.message)
+      toast.error('로그인에 실패하였습니다. 이메일과 비밀번호를 확인해주세요.');
     },
   });
 
