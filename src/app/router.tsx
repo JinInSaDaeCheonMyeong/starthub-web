584dd30390ba58c4d8232e1d9669a91bf373b027
yujunggg <a90134627@gmail.com>
2025-07-05 23:02:54 +0900

    feat: add router my-page #39

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import SignInPage from "@/pages/SignIn";
 import SignUpPage from "@/pages/SignUp";
+import MyPage from "@/pages/MyPage";
 
 const Router = () => {
   return(
@@ -8,6 +9,7 @@ const Router = () => {
       <Routes>
         <Route path="/sign-up" element={<SignUpPage/>}/>
         <Route path="/sign-in" element={<SignInPage/>}/>
+        <Route path="/my-page" element={<MyPage/>}/>
       </Routes>
     </BrowserRouter>
   )
