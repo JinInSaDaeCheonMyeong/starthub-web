f3ac4c2071e96074366a576567d969555c9091d3
dlskl <skyandme77@gmail.com>
2025-07-07 20:42:26 +0900

    feat : add footer and banner #10

---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,16 @@
+import Header from "@/shared/ui/Header";
+import Banner from "@/shared/ui/Banner";
+import Footer from "@/shared/ui/Footer";
+
+
+const MainPage = () => {
+  return (
+    <>
+      <Header />
+        <Banner />
+      <Footer/>
+    </>
+  );
+};
+
+export default MainPage;
