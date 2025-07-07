258eb9118f9bc8437b03af338596e8e2eca88f33
yujunggg <a90134627@gmail.com>
2025-07-07 14:33:28 +0900

    feat: add AgreementSection component with props for checkbox handling

--- src/features/auth/signUp/ui/SignUpForm/AgreementSection/index.tsx
+++ src/features/auth/signUp/ui/SignUpForm/AgreementSection/index.tsx
@@ -1,34 +1,25 @@
 import * as S from "./style";
 import { StartHubCheckBox } from "@/shared/ui";
-import { useState } from "react";
+import React from "react";
 import { SIGNUP_AGREE_ITEMS } from "@/features/auth/signUp/constants/signup.constants";
 
-const AgreementSection = () => {
-  const [isAllChecked, setIsAllChecked] = useState(false);
-  const [checkedItems, setCheckedItems] = useState<boolean[]>(
-    new Array(SIGNUP_AGREE_ITEMS.length).fill(false)
-  );
+interface AgreementSectionProps {
+  checkedItems: boolean[];
+  isAllChecked: boolean;
+  onAllCheck: (checked: boolean) => void;
+  onSingleCheck: (index: number, checked: boolean) => void;
+}
 
-  // 전체 동의 체크박스 핸들러
-  const handleAllCheck = (checked: boolean) => {
-    setIsAllChecked(checked);
-    setCheckedItems(new Array(SIGNUP_AGREE_ITEMS.length).fill(checked));
-  };
-
-  // 개별 체크박스 핸들러
-  const handleSingleCheck = (index: number, checked: boolean) => {
-    const newCheckedItems = [...checkedItems];
-    newCheckedItems[index] = checked;
-    setCheckedItems(newCheckedItems);
-
-    const allChecked = newCheckedItems.every((item) => item);
-    setIsAllChecked(allChecked);
-  };
-
+const AgreementSection: React.FC<AgreementSectionProps> = ({
+  checkedItems,
+  isAllChecked,
+  onAllCheck,
+  onSingleCheck,
+}) => {
   return (
     <>
       <S.CheckboxContainer>
-        <StartHubCheckBox checked={isAllChecked} onChange={handleAllCheck} />
+        <StartHubCheckBox checked={isAllChecked} onChange={onAllCheck} />
         <S.CheckboxLabelMain>전체 동의</S.CheckboxLabelMain>
       </S.CheckboxContainer>
       <S.Divider />
@@ -36,7 +27,7 @@ const AgreementSection = () => {
         <S.CheckboxContainer key={index}>
           <StartHubCheckBox
             checked={checkedItems[index]}
-            onChange={(checked) => handleSingleCheck(index, checked)}
+            onChange={(checked) => onSingleCheck(index, checked)}
           />
           <S.CheckboxLabel>{agree}</S.CheckboxLabel>
         </S.CheckboxContainer>
