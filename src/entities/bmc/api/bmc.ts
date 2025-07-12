520208ada4bb945a03c7ab9e56bdd07efa3c7c7d
서진교 <oygnijoes0209@gmail.com>
2025-07-13 05:22:37 +0900

    feat: add bmc api

---
+++ src/entities/bmc/api/bmc.ts
@@ -0,0 +1,37 @@
+import StartHubAxios from '../../../shared/api/customAxios/StartHubAxios'
+import { BmcSessionResponse, CreateSessionRequest, CreateAnswerResponse, CreateAnswerRequest, ModifyBmcRequest, ModifyBmcResponse, CreateBmcRequest, CreateBmcResponse, SessionDetailResponse, QuestionListResponse, CanvasesResponse, CanvasesDetailResponse, DeleteCanvasesResponse, ModifyHistoryResponse } from '../model/types'
+
+export const bmcApi = {
+  getSessions: (): Promise<BmcSessionResponse> =>
+    StartHubAxios.get('/bmc/sessions'),
+
+  createSessions: (data: CreateSessionRequest): Promise<CreateAnswerResponse> =>
+    StartHubAxios.post('/bmc/sessions', data),
+
+  createAnswer: (data: CreateAnswerRequest): Promise<CreateAnswerResponse> =>
+    StartHubAxios.post('/bmc/sessions/answer', data),
+
+  modifyBmc: (data: ModifyBmcRequest): Promise<ModifyBmcResponse> =>
+    StartHubAxios.post('/bmc/modify', data),
+  
+  createBmc: (data: CreateBmcRequest): Promise<CreateBmcResponse> =>
+    StartHubAxios.post('/bmc/generate', data),
+
+  getSessionDetail: (sessionId: string): Promise<SessionDetailResponse> =>
+    StartHubAxios.get(`/bmc/sessions/${sessionId}`),
+
+  getQuestions: (): Promise<QuestionListResponse> =>
+    StartHubAxios.get('/bmc/questions'),
+
+  getCanvases: (): Promise<CanvasesResponse> =>
+    StartHubAxios.get('bmc/canvases'),
+
+  getCanvasesDetail: (id: string): Promise<CanvasesDetailResponse> =>
+    StartHubAxios.get(`/bmc/canvases/${id}`),
+
+  deleteCanvases: (id: string): Promise<DeleteCanvasesResponse> =>
+    StartHubAxios.delete(`/bmc/canvases/${id}`),
+
+  getBmcModifyHistory: (id: string): Promise<ModifyHistoryResponse> =>
+    StartHubAxios.get(`/bmc/canvases/${id}/history`)
+}
