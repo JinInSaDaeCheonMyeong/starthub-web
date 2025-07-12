461b602d2061a679476e2c8e39f167947afb1551
dlskl <skyandme77@gmail.com>
2025-07-12 14:05:40 +0900

    feat : add main page #49

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,16 +1,37 @@
 import Header from "@/shared/ui/Header";
 import Banner from "@/shared/ui/Banner";
 import Footer from "@/shared/ui/Footer";
-
+import MainMenu from "@/features/mainMenu/ui";
+import MainContent from "./ui/MainContent";
+import styled from "styled-components";
 
 const MainPage = () => {
   return (
-    <>
+    <PageLayout>
       <Header />
-        <Banner />
-      <Footer/>
-    </>
+      <Banner />
+      <CenteredBox>
+        <MainMenu />
+      </CenteredBox>
+      <CenteredBox>
+        <MainContent />
+      </CenteredBox>
+      <Footer />
+    </PageLayout>
   );
 };
 
 export default MainPage;
+
+
+const PageLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const CenteredBox = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
