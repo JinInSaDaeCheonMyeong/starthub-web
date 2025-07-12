e64424f7eea8ef8fdf98caefc46fe364ee975361
dlskl <skyandme77@gmail.com>
2025-07-12 18:45:29 +0900

    chore : divide style and tsx #49

---
+++ src/pages/Main/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const PageLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const CenteredBox = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
