358cc5f798e2a37b477816448952c4c4bc0c680e
dlskl <skyandme77@gmail.com>
2025-07-13 01:23:10 +0900

    fix : divide style and tsx #56

--- src/features/mainMenu/ui/index.tsx
+++ src/features/mainMenu/ui/index.tsx
@@ -1,12 +1,11 @@
-import styled from "styled-components";
 import { ReactComponent as BMCIcon } from "@assets/icons/bag.svg";
 import { ReactComponent as ChartIcon } from "@assets/icons/chart.svg";
 import { ReactComponent as RocketIcon } from "@assets/icons/rocketMenu.svg";
 import { ReactComponent as HumanIcon } from "@assets/icons/human.svg";
 import { ReactComponent as BuildingIcon } from "@assets/icons/building.svg";
 import { ReactComponent as Members } from "@assets/icons/members.svg";
-import { StartHubColors, StartHubFont } from "@/shared/design";
 import { useNavigate } from "react-router-dom";
+import * as S from "./style";
 
 const MainMenu = () => {
   const menuItems = [
@@ -20,9 +19,9 @@ const MainMenu = () => {
   const navigate = useNavigate();
 
   return (
-    <MenuContainer>
+    <S.MenuContainer>
       {menuItems.map((item, idx) => (
-        <MenuButton
+        <S.MenuButton
           key={idx}
           role="button"
           aria-label={item.label}
@@ -30,47 +29,10 @@ const MainMenu = () => {
         >
           {item.icon}
           <p>{item.label}</p>
-        </MenuButton>
+        </S.MenuButton>
       ))}
-    </MenuContainer>
+    </S.MenuContainer>
   );
 };
 
 export default MainMenu;
-
-const MenuContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  gap: 44px;
-  padding: 40px 0 50px;
-`;
-
-const MenuButton = styled.div`
-  width: fit-content;
-  height: fit-content;
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  cursor: pointer;
-
-  &:hover {
-    background-color: ${StartHubColors.Gray4};
-    border-radius: 10px;
-    p {
-      color: ${StartHubColors.Black1};
-    }
-  }
-
-  svg {
-    width: 50px;
-    height: 50px;
-  }
-
-  p {
-    margin-top: 8px;
-    ${StartHubFont.Pretendard.Body2.Medium}
-    color: ${StartHubColors.Gray2};
-  }
-`;
