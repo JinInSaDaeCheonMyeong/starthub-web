5c4c4dfa49efc40fcd74d79d6fdf11f6ff256a6d
dlskl <skyandme77@gmail.com>
2025-07-12 18:45:29 +0900

    chore : divide style and tsx #49

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -3,35 +3,23 @@ import Banner from "@/shared/ui/Banner";
 import Footer from "@/shared/ui/Footer";
 import MainMenu from "@/features/mainMenu/ui";
 import MainContent from "./ui/MainContent";
-import styled from "styled-components";
+import * as S from "./style"
 
 const MainPage = () => {
   return (
-    <PageLayout>
+    <S.PageLayout>
       <Header />
       <Banner />
-      <CenteredBox>
+      <S.CenteredBox>
         <MainMenu />
-      </CenteredBox>
-      <CenteredBox>
+      </S.CenteredBox>
+      <S.CenteredBox>
         <MainContent />
-      </CenteredBox>
+      </S.CenteredBox>
       <Footer />
-    </PageLayout>
+    </S.PageLayout>
   );
 };
 
 export default MainPage;
 
-
-const PageLayout = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const CenteredBox = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 100%;
-`;
