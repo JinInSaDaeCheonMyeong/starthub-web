9278e3193918ed69aeea430a040300bcfd0ff2e8
dlskl <skyandme77@gmail.com>
2025-07-13 01:03:08 +0900

    fix : add menu router #56

--- src/features/boxMenu/ui/index.tsx
+++ src/features/boxMenu/ui/index.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import * as S from "./style"
+import * as S from "./style";
 
 const BoxMenu = () => {
   const navigate = useNavigate();
@@ -12,7 +12,15 @@ const BoxMenu = () => {
           <div>AI가 내 아이템을 보고 BMC를 설계해줘요!</div>
           <span>내 BMC 보기</span>
         </S.BoxContent>
-        <S.BoxButton $bgColor="#FF71AF" $hvColor="#e85c9c" onClick={()=>{navigate("/")}}>바로가기</S.BoxButton>
+        <S.BoxButton
+          $bgColor="#FF71AF"
+          $hvColor="#e85c9c"
+          onClick={() => {
+            navigate("/bmc");
+          }}
+        >
+          바로가기
+        </S.BoxButton>
       </S.PinkBox>
 
       <div>
@@ -21,7 +29,15 @@ const BoxMenu = () => {
             경쟁사 분석
             <div>AI가 내 아이템을 보고 시장조사를 해줘요!</div>
           </S.BoxContent>
-          <S.BoxButton $bgColor="#64DD91" $hvColor="#4fc97d" onClick={()=>{navigate("/")}}>바로가기</S.BoxButton>
+          <S.BoxButton
+            $bgColor="#64DD91"
+            $hvColor="#4fc97d"
+            onClick={() => {
+              navigate("/competitor");
+            }}
+          >
+            바로가기
+          </S.BoxButton>
         </S.GreenBox>
 
         <S.BlueBox>더 좋은 서비스를 경험해보세요!</S.BlueBox>
@@ -33,12 +49,34 @@ const BoxMenu = () => {
             기업 등록하기
             <div>AI가 내 아이템을 보고 시장조사를 해줘요!</div>
           </S.BoxContent>
-          <S.BoxButton $bgColor="#D290FB" $hvColor="#b875df" onClick={()=>{navigate("/")}}>바로가기</S.BoxButton>
+          <S.BoxButton
+            $bgColor="#D290FB"
+            $hvColor="#b875df"
+            onClick={() => {
+              navigate("/my-profile");
+            }}
+          >
+            바로가기
+          </S.BoxButton>
         </S.PurpleBox>
 
         <div style={{ display: "flex", gap: "14px", marginTop: "14px" }}>
-          <S.GrayBox $hvColor="#cccccc" onClick={()=>{navigate("/")}}>최신 공고</S.GrayBox>
-          <S.GrayBox $hvColor="#cccccc" onClick={()=>{navigate("/")}}>스타트업 채용</S.GrayBox>
+          <S.GrayBox
+            $hvColor="#cccccc"
+            onClick={() => {
+              navigate("/notices");
+            }}
+          >
+            최신 공고
+          </S.GrayBox>
+          <S.GrayBox
+            $hvColor="#cccccc"
+            onClick={() => {
+              navigate("/team-building");
+            }}
+          >
+            스타트업 채용
+          </S.GrayBox>
         </div>
       </div>
     </S.BoxContainer>
--- src/features/mainMenu/ui/index.tsx
+++ src/features/mainMenu/ui/index.tsx
@@ -4,23 +4,30 @@ import { ReactComponent as ChartIcon } from "@assets/icons/chart.svg";
 import { ReactComponent as RocketIcon } from "@assets/icons/rocketMenu.svg";
 import { ReactComponent as HumanIcon } from "@assets/icons/human.svg";
 import { ReactComponent as BuildingIcon } from "@assets/icons/building.svg";
-import { ReactComponent as Members} from "@assets/icons/members.svg";
+import { ReactComponent as Members } from "@assets/icons/members.svg";
 import { StartHubColors, StartHubFont } from "@/shared/design";
+import { useNavigate } from "react-router-dom";
 
 const MainMenu = () => {
   const menuItems = [
-    { icon: <BMCIcon />, label: "BMC 제작" },
-    { icon: <ChartIcon />, label: "경쟁사 분석" },
-    { icon: <RocketIcon />, label: "최신 공고 보러가기" },
-    { icon: <HumanIcon />, label: "청년 맞춤 공고" },
-    { icon: <Members/>, label: "창업 멤버 모집하기"},
-    { icon: <BuildingIcon />, label: "기업 등록하기"},
+    { icon: <BMCIcon />, label: "BMC 제작", path: "/bmc" },
+    { icon: <ChartIcon />, label: "경쟁사 분석", path: "/competitor" },
+    { icon: <RocketIcon />, label: "최신 공고 보러가기", path: "/notice" },
+    { icon: <HumanIcon />, label: "청년 맞춤 공고", path: "/notice" },
+    { icon: <Members />, label: "창업 멤버 모집하기", path: "team-building" },
+    { icon: <BuildingIcon />, label: "기업 등록하기", path: "/my-business" },
   ];
+  const navigate = useNavigate();
 
   return (
     <MenuContainer>
       {menuItems.map((item, idx) => (
-        <MenuButton key={idx} role="button" aria-label={item.label}>
+        <MenuButton
+          key={idx}
+          role="button"
+          aria-label={item.label}
+          onClick={() => navigate(item.path)}
+        >
           {item.icon}
           <p>{item.label}</p>
         </MenuButton>
@@ -36,23 +43,29 @@ const MenuContainer = styled.div`
   flex-wrap: wrap;
   justify-content: center;
   gap: 44px;
-  padding: 50px 0 100px;
+  padding: 40px 0 50px;
 `;
 
 const MenuButton = styled.div`
+  width: fit-content;
+  height: fit-content;
+  padding: 20px;
   display: flex;
   flex-direction: column;
   align-items: center;
   cursor: pointer;
-  transition: transform 0.2s ease;
 
   &:hover {
-    transform: scale(1.05);
+    background-color: ${StartHubColors.Gray4};
+    border-radius: 10px;
+    p {
+      color: ${StartHubColors.Black1};
+    }
   }
 
   svg {
-    width: 70px;
-    height: 70px;
+    width: 50px;
+    height: 50px;
   }
 
   p {
