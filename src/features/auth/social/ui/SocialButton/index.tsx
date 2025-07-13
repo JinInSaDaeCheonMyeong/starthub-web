de2cb96b71e7d499e348faad30043a73abf77e28
서진교 <oygnijoes0209@gmail.com>
2025-07-14 00:31:39 +0900

    feat: add Google OAuth #43

--- src/features/auth/social/ui/SocialButton/index.tsx
+++ src/features/auth/social/ui/SocialButton/index.tsx
@@ -1,15 +1,18 @@
 import { StartHubColors, StartHubFont } from "@/shared/design";
 import { Google, Naver, Apple } from "@/assets/icons";
 import { StartHubButton } from "@/shared/ui";
+import { useGoogleSignIn } from "@/features/auth/social/model/useGoogleSignIn";
 
 const SocialButton = () => {
+  const { handleGoogleSignIn } = useGoogleSignIn();
+
   return (
     <>
       <StartHubButton
         icon={<Google style={{ width: "18px", height: "18px" }} />}
         text="Google로 로그인"
         backgroundColor={StartHubColors.White1}
-        onClick={() => {}}
+        onClick={handleGoogleSignIn}
         typography={StartHubFont.Pretendard.Caption1.Medium}
         width={320}
         height={50}
@@ -25,7 +28,7 @@ const SocialButton = () => {
         icon={<Naver style={{ width: "16px", height: "16px" }} />}
         text="네이버로 로그인"
         backgroundColor="#00C300"
-        onClick={() => {}}
+        onClick={()=>{}}
         typography={StartHubFont.Pretendard.Caption1.Medium}
         width={320}
         height={50}
@@ -41,7 +44,7 @@ const SocialButton = () => {
         icon={<Apple style={{ width: "14px", height: "17px" }} />}
         text="Apple로 로그인"
         backgroundColor={StartHubColors.Black1}
-        onClick={() => {}}
+        onClick={()=>{}}
         typography={StartHubFont.Pretendard.Caption1.Medium}
         width={320}
         height={50}
