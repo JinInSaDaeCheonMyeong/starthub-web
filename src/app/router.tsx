5ac5393df99711305de39ef35f84e9a56f80c1b9
서진교 <oygnijoes0209@gmail.com>
2025-07-14 00:28:48 +0900

    feat: add Callback page #43

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,33 +1,30 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignInPage from "@/pages/SignIn";
 import SignUpPage from "@/pages/SignUp";
 import MainPage from "@/pages/Main";
-import Onboarding from "@/pages/Onboarding";
 import NoticePage from "@/pages/Notice";
 import CallbackPage from "@/pages/Callback";
 import Onboarding from "@/pages/Onboarding";
 import MyProfile from "@/pages/MyProfile";
 
 const Router = () => {
-  return(
+  return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage/>}/>
-        <Route path="/sign-up" element={<SignUpPage/>}/>
-        <Route path="/sign-in" element={<SignInPage/>}/>
-        <Route path="/onboarding" element={<Onboarding/>}/>
-        <Route path="/notices" element={<NoticePage/>}/>
-        <Route path="/onboarding" element={<Onboarding/>}/>
-        {/* <Route path="/notices" element={}/> */}
+        <Route path="/" element={<MainPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/notices" element={<NoticePage />} />
         {/* <Route path="/competitor" element={}/> */}
         {/* <Route path="/team-building" element={}/> */}
         {/* <Route path="/bmc" element={}/> */}
         <Route path="/my-profile" element={<MyProfile/>}/>
         {/* <Route path="/my-business" element={}/> */}
-        <Route path="/oauth/callback" element={<CallbackPage/>}/>
+        <Route path="/oauth/callback" element={<CallbackPage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 };
 
 export default Router;
