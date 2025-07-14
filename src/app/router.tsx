82baeb66f8bfeea1b0a61f59f49f57fb173ff175
dlskl <skyandme77@gmail.com>
2025-07-14 10:30:31 +0900

    refactor : fix router and header #76

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route} from "react-router-dom";
 import SignInPage from "@/pages/SignIn";
 import SignUpPage from "@/pages/SignUp";
 import MainPage from "@/pages/Main";
+import NoticePage from "@/pages/Notice";
 
 const Router = () => {
   return(
@@ -10,7 +11,7 @@ const Router = () => {
         <Route path="/" element={<MainPage/>}/>
         <Route path="/sign-up" element={<SignUpPage/>}/>
         <Route path="/sign-in" element={<SignInPage/>}/>
-        {/* <Route path="/notices" element={}/> */}
+        <Route path="/notices" element={<NoticePage/>}/>
         {/* <Route path="/competitor" element={}/> */}
         {/* <Route path="/team-building" element={}/> */}
         {/* <Route path="/bmc" element={}/> */}
