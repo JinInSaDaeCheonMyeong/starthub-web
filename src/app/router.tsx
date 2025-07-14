2d736d7bf8c17b16d903728b33e24b84b7c2545c
yujunggg <a90134627@gmail.com>
2025-07-14 17:43:15 +0900

    chore: add route

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -2,8 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignInPage from "@/pages/SignIn";
 import SignUpPage from "@/pages/SignUp";
 import MainPage from "@/pages/Main";
-import NoticePage from "@/pages/Notice";
-import CallbackPage from "@/pages/Callback";
 import Onboarding from "@/pages/Onboarding";
 import MyProfile from "@/pages/MyProfile";
 
@@ -14,14 +12,14 @@ const Router = () => {
         <Route path="/" element={<MainPage/>}/>
         <Route path="/sign-up" element={<SignUpPage/>}/>
         <Route path="/sign-in" element={<SignInPage/>}/>
-        <Route path="/notices" element={<NoticePage/>}/>
         <Route path="/onboarding" element={<Onboarding/>}/>
+        {/* <Route path="/notices" element={}/> */}
         {/* <Route path="/competitor" element={}/> */}
         {/* <Route path="/team-building" element={}/> */}
         {/* <Route path="/bmc" element={}/> */}
         <Route path="/my-profile" element={<MyProfile/>}/>
         {/* <Route path="/my-business" element={}/> */}
-        <Route path="/oauth/callback" element={<CallbackPage/>}/>
+        {/* <Route path="/oauth/callback" element={}/> */}
       </Routes>
     </BrowserRouter>
   );
