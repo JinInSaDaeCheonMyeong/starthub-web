843d13940c1cdce9d77dc559badcfaa7247eb0a5
dlskl <skyandme77@gmail.com>
2025-07-14 14:00:35 +0900

    fix: fix common components #73

--- src/shared/ui/SectionBlock/index.tsx
+++ src/shared/ui/SectionBlock/index.tsx
@@ -3,26 +3,29 @@ import { StartHubFont } from "@/shared/design";
 import { ReactComponent as Arrow } from "@assets/icons/arrow.svg";
 
 const SectionBlock = ({
-    title,
-    children,
-  }: {
-    title: string;
-    children: React.ReactNode;
-  }) => (
-    <>
-      <LabelSection>
-        {title}
-        <Arrow />
-      </LabelSection>
-      {children}
-    </>
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) => (
+  <>
+    <LabelSection>
+      <p>{title}</p>
+      <Arrow />
+    </LabelSection>
+    {children}
+  </>
 );
 
-export default SectionBlock
+export default SectionBlock;
 
 export const LabelSection = styled.div`
-  ${StartHubFont.Pretendard.Headlines1.SemiBold}
+  ${StartHubFont.Pretendard.Body1.Medium}
   display: flex;
   align-items: center;
   margin-bottom: 20px;
+  p {
+    margin-right: 10px;
+  }
 `;
