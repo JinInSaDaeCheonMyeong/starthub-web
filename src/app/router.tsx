4884cd4ef24725d638f50b967e41c4a1f39bd286
yujunggg <a90134627@gmail.com>
2025-07-14 17:43:15 +0900

    chore: add route

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -11,17 +11,17 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/sign-up" element={<SignUpPage />} />
-        <Route path="/sign-in" element={<SignInPage />} />
-        <Route path="/onboarding" element={<Onboarding />} />
-        <Route path="/notices" element={<NoticePage />} />
+        <Route path="/" element={<MainPage/>}/>
+        <Route path="/sign-up" element={<SignUpPage/>}/>
+        <Route path="/sign-in" element={<SignInPage/>}/>
+        <Route path="/notices" element={<NoticePage/>}/>
+        <Route path="/onboarding" element={<Onboarding/>}/>
         {/* <Route path="/competitor" element={}/> */}
         {/* <Route path="/team-building" element={}/> */}
         {/* <Route path="/bmc" element={}/> */}
         <Route path="/my-profile" element={<MyProfile/>}/>
         {/* <Route path="/my-business" element={}/> */}
-        <Route path="/oauth/callback" element={<CallbackPage />} />
+        <Route path="/oauth/callback" element={<CallbackPage/>}/>
       </Routes>
     </BrowserRouter>
   );
