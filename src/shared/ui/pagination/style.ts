ee5410b5a1290778c21cfc0c5538af091f032919
dlskl <skyandme77@gmail.com>
2025-07-17 21:56:39 +0900

    feat : add pagination component #89

---
+++ src/shared/ui/pagination/style.ts
@@ -0,0 +1,34 @@
+import { StartHubColors, StartHubFont } from "@/shared/design";
+import styled from "styled-components";
+
+export const pagination = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 100px;
+  margin-bottom: 30px;
+  gap: 20px;
+  p {
+    ${StartHubFont.Pretendard.Caption1.Regular}
+    color: ${StartHubColors.Gray2};
+  }
+
+  svg {
+    width: 24px;
+    height: 24px;
+    &:hover {
+      fill: black;
+    }
+  }
+`;
+export const PageNumber = styled.p<{ isActive: boolean }>`
+  cursor: pointer;
+  padding-bottom: 4px;
+  ${StartHubFont.Pretendard.Caption1.Regular}
+  color: ${({ isActive }) =>
+    isActive ? StartHubColors.Black1 : StartHubColors.Gray2};
+  transition: all 0.2s ease;
+
+  &:hover {
+    color: ${StartHubColors.Black1};
+  }
+`;
