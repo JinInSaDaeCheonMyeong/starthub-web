44d34be8f4ae038610d8c500b1260e6f41f322cf
yujunggg <a90134627@gmail.com>
2025-07-18 10:33:30 +0900

    feat: create Main MyBusiness page

---
+++ src/pages/MyBusiness/index.tsx
@@ -0,0 +1,13 @@
+import Header from '@/widgets/Header'
+import BusinessProfileForm from '@/features/profile/business/businessForm'
+
+const MyBusiness = () => {
+  return (
+    <div>
+      <Header/>
+      <BusinessProfileForm />
+    </div>
+  )
+}
+
+export default MyBusiness
