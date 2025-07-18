25408bd66a00a387d5590d68e45a34d75ad6f26a
yujunggg <a90134627@gmail.com>
2025-07-18 10:33:45 +0900

    feat: create profileViewMode

---
+++ src/features/profile/users/profileViewMode/index.tsx
@@ -0,0 +1,30 @@
+import * as S from "./style";
+import type { ProfileFormData } from "@/entities/user/model/types";
+
+interface ProfileViewModeProps {
+  formData?: ProfileFormData;
+}
+
+const ProfileViewMode = ({ formData }: ProfileViewModeProps) => {
+
+  return (
+    <div>
+      <S.InfoRow>
+        <S.InfoLabel>성별</S.InfoLabel>
+        <S.InfoValue>{formData?.gender || '-'}</S.InfoValue>
+      </S.InfoRow>
+      
+      <S.InfoRow>
+        <S.InfoLabel>생년월일</S.InfoLabel>
+        <S.InfoValue>{formData?.birthDate || '-'}</S.InfoValue>
+      </S.InfoRow>
+      
+      <S.InfoRow>
+        <S.InfoLabel>이메일 주소</S.InfoLabel>
+        <S.InfoValue>{formData?.email || '-'}</S.InfoValue>
+      </S.InfoRow>
+    </div>
+  );
+};
+
+export default ProfileViewMode;
