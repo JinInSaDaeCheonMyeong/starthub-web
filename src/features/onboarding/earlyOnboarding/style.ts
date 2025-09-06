2382acdf921a39522b84da7e7b8330ee99476455
yujunggg <a90134627@gmail.com>
2025-09-06 20:35:11 +0900

    feat: create earlyOnboarding

--- src/features/onboarding/earlyOnboarding/style.ts
+++ src/features/onboarding/earlyOnboarding/style.ts
@@ -9,12 +9,7 @@ export const Section = styled.div`
 export const SectionTitle = styled.h3`
   font: ${StartHubFont.Pretendard.Body2.Medium};
   color: ${StartHubColors.Black1};
-  margin-bottom: 10px;
-
-  span {
-    color: ${StartHubColors.Primary};
-    font-size: 14px;
-  }
+  margin-bottom: 8px;
 `;
 
 export const SectionDescription = styled.p`
