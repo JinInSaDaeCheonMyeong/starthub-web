cb0ac7d57f8377f48adad3193a1099b1fb8c6c58
yujunggg <a90134627@gmail.com>
2025-09-08 08:58:43 +0900

    feat: create preOnboarding page

---
+++ src/features/onboarding/preOnboarding/index.tsx
@@ -0,0 +1,33 @@
+import { StartHubTextField } from "@/shared/ui";
+import * as S from "./style";
+import { useState } from "react";
+
+const EarlyOnboarding = () => {
+  const [formData, setFormData] = useState({
+    companyName: "",
+    companyDescription: "",
+    employeeCount: 0,
+    companyWebsite: "",
+    startupLocation: "",
+    annualRevenue: 0
+  });
+
+  return (
+    <>
+      <S.Section>
+        <S.SectionTitle>창업 위치</S.SectionTitle>
+        <StartHubTextField
+          type="text"
+          value={formData.startupLocation}
+          placeholder="창업 위치를 입력해주세요"
+          onChange={(e) =>
+            setFormData({ ...formData, startupLocation: e.target.value })
+          }
+          customStyle={{ height: 50, width: "100%" }}
+        />
+      </S.Section>
+    </>
+  );
+};
+
+export default EarlyOnboarding;
