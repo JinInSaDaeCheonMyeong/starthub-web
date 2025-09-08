5a77d245c6609a9a7896c7117d878cf624b24bda
dlskl <skyandme77@gmail.com>
2025-09-08 15:51:57 +0900

    refactor : refactoring main page ui

--- src/features/boxMenu/ui/index.tsx
+++ src/features/boxMenu/ui/index.tsx
@@ -1,21 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import * as S from "./style";
-import { toast } from "react-toastify";
 
 const BoxMenu = () => {
   const navigate = useNavigate();
-  
-   const handleNavigate = (path: string) => { 
-    if (path === "/competitor") {
-      toast.error("경쟁사 분석 기능은 현재 준비 중입니다. 곧 만나보실 수 있어요!");
-      return;
-    }
-    if (path === "/team-building") {
-      toast.error("창업 멤버 모집 기능은 현재 준비 중입니다. 곧 만나보실 수 있어요!");
-      return;
-    }
-    navigate(path);
-  };
 
   return (
     <S.BoxContainer>
@@ -28,9 +15,7 @@ const BoxMenu = () => {
         <S.BoxButton
           $bgColor="#FF71AF"
           $hvColor="#e85c9c"
-          onClick={() => {
-            handleNavigate("/bmc");
-          }}
+          onClick={() => {navigate("/bmc")}}
         >
           바로가기
         </S.BoxButton>
@@ -46,7 +31,7 @@ const BoxMenu = () => {
             $bgColor="#64DD91"
             $hvColor="#4fc97d"
             onClick={() => {
-              handleNavigate("/competitor");
+              navigate("/competitor");
             }}
           >
             바로가기
@@ -59,38 +44,20 @@ const BoxMenu = () => {
       <div>
         <S.PurpleBox>
           <S.BoxContent>
-            기업 등록하기
-            <div>AI가 내 아이템을 보고 시장조사를 해줘요!</div>
+            공고 보기
+            <div>지금 업데이트되는 공고들을 확인해 보세요!</div>
+            <span>내가 관심 있어 한 공고 보기</span>
           </S.BoxContent>
           <S.BoxButton
             $bgColor="#D290FB"
             $hvColor="#b875df"
             onClick={() => {
-              handleNavigate("/my-profile");
+              navigate("/my-profile");
             }}
           >
             바로가기
           </S.BoxButton>
         </S.PurpleBox>
-
-        <div style={{ display: "flex", gap: "14px", marginTop: "14px" }}>
-          <S.GrayBox
-            $hvColor="#cccccc"
-            onClick={() => {
-              handleNavigate("/notices");
-            }}
-          >
-            최신 공고
-          </S.GrayBox>
-          <S.GrayBox
-            $hvColor="#cccccc"
-            onClick={() => {
-              handleNavigate("/team-building");
-            }}
-          >
-            스타트업 채용
-          </S.GrayBox>
-        </div>
       </div>
     </S.BoxContainer>
   );
