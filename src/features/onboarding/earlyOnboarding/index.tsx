85b459f345b771776a55bc10ce5b47e9a7405b23
yujunggg <a90134627@gmail.com>
2025-09-08 09:11:30 +0900

    chore: 불필요한 괄호 생략

--- src/features/onboarding/earlyOnboarding/index.tsx
+++ src/features/onboarding/earlyOnboarding/index.tsx
@@ -9,85 +9,83 @@ const EarlyOnboarding = () => {
     employeeCount: 0,
     companyWebsite: "",
     startupLocation: "",
-    annualRevenue: 0
+    annualRevenue: 0,
   });
 
   return (
-    <>
-      <S.Section>
-        <S.SectionTitle>회사명</S.SectionTitle>
-        <StartHubTextField
-          type="text"
-          value={formData.companyName}
-          placeholder="회사명을 입력해주세요"
-          onChange={(e) =>
-            setFormData({ ...formData, companyName: e.target.value })
-          }
-          customStyle={{ height: 50, width: "100%" }}
-        />
-        <S.SectionTitle>기업 설명</S.SectionTitle>
-        <StartHubTextField
-          type="text"
-          value={formData.companyDescription}
-          placeholder="기업을 설명해주세요"
-          onChange={(e) =>
-            setFormData({ ...formData, companyDescription: e.target.value })
-          }
-          customStyle={{ height: 50, width: "100%" }}
-        />
-        <S.SectionTitle>기업 인원</S.SectionTitle>
-        <StartHubTextField
-          type="number"
-          value={
-            formData.employeeCount === 0 ? "" : String(formData.employeeCount)
-          }
-          placeholder="기업 인원을 입력해주세요"
-          onChange={(e) =>
-            setFormData({
-              ...formData,
-              employeeCount: e.target.value === "" ? 0 : Number(e.target.value),
-            })
-          }
-          customStyle={{ height: 50, width: "100%" }}
-        />
+    <S.Section>
+      <S.SectionTitle>회사명</S.SectionTitle>
+      <StartHubTextField
+        type="text"
+        value={formData.companyName}
+        placeholder="회사명을 입력해주세요"
+        onChange={(e) =>
+          setFormData({ ...formData, companyName: e.target.value })
+        }
+        customStyle={{ height: 50, width: "100%" }}
+      />
+      <S.SectionTitle>기업 설명</S.SectionTitle>
+      <StartHubTextField
+        type="text"
+        value={formData.companyDescription}
+        placeholder="기업을 설명해주세요"
+        onChange={(e) =>
+          setFormData({ ...formData, companyDescription: e.target.value })
+        }
+        customStyle={{ height: 50, width: "100%" }}
+      />
+      <S.SectionTitle>기업 인원</S.SectionTitle>
+      <StartHubTextField
+        type="number"
+        value={
+          formData.employeeCount === 0 ? "" : String(formData.employeeCount)
+        }
+        placeholder="기업 인원을 입력해주세요"
+        onChange={(e) =>
+          setFormData({
+            ...formData,
+            employeeCount: e.target.value === "" ? 0 : Number(e.target.value),
+          })
+        }
+        customStyle={{ height: 50, width: "100%" }}
+      />
 
-        <S.SectionTitle>기업 사이트</S.SectionTitle>
-        <StartHubTextField
-          type="text"
-          value={formData.companyWebsite}
-          placeholder="기업 사이트 주소를 입력해주세요"
-          onChange={(e) =>
-            setFormData({ ...formData, companyWebsite: e.target.value })
-          }
-          customStyle={{ height: 50, width: "100%" }}
-        />
-        <S.SectionTitle>창업 위치</S.SectionTitle>
-        <StartHubTextField
-          type="text"
-          value={formData.startupLocation}
-          placeholder="창업 위치를 입력해주세요"
-          onChange={(e) =>
-            setFormData({ ...formData, startupLocation: e.target.value })
-          }
-          customStyle={{ height: 50, width: "100%" }}
-        />
-        <S.SectionTitle>연매출액</S.SectionTitle>
-        <StartHubTextField
-          type="number"
-          value={
-            formData.annualRevenue === 0 ? "" : String(formData.annualRevenue)
-          }
-          placeholder="연매출액을 입력해주세요"
-          onChange={(e) =>
-            setFormData({
-              ...formData,
-              annualRevenue: e.target.value === "" ? 0 : Number(e.target.value),
-            })
-          }
-          customStyle={{ height: 50, width: "100%" }}
-        />
-      </S.Section>
-    </>
+      <S.SectionTitle>기업 사이트</S.SectionTitle>
+      <StartHubTextField
+        type="text"
+        value={formData.companyWebsite}
+        placeholder="기업 사이트 주소를 입력해주세요"
+        onChange={(e) =>
+          setFormData({ ...formData, companyWebsite: e.target.value })
+        }
+        customStyle={{ height: 50, width: "100%" }}
+      />
+      <S.SectionTitle>창업 위치</S.SectionTitle>
+      <StartHubTextField
+        type="text"
+        value={formData.startupLocation}
+        placeholder="창업 위치를 입력해주세요"
+        onChange={(e) =>
+          setFormData({ ...formData, startupLocation: e.target.value })
+        }
+        customStyle={{ height: 50, width: "100%" }}
+      />
+      <S.SectionTitle>연매출액</S.SectionTitle>
+      <StartHubTextField
+        type="number"
+        value={
+          formData.annualRevenue === 0 ? "" : String(formData.annualRevenue)
+        }
+        placeholder="연매출액을 입력해주세요"
+        onChange={(e) =>
+          setFormData({
+            ...formData,
+            annualRevenue: e.target.value === "" ? 0 : Number(e.target.value),
+          })
+        }
+        customStyle={{ height: 50, width: "100%" }}
+      />
+    </S.Section>
   );
 };
 
