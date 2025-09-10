678db3ca1eb783377e4978a258899bffbb718b1c
yujunggg <a90134627@gmail.com>
2025-09-10 21:18:19 +0900

    remove: 기존 마이페이지

--- src/pages/MyProfile/index.tsx
+++ src/pages/MyProfile/index.tsx
@@ -1,11 +1,9 @@
 import Header from "@/widgets/Header"
-import ProfileForm from "@/features/profile/users/profileForm"
 
 const MyProfile = () => {
   return (
     <>
         <Header/>
-        <ProfileForm/>
     </>
   )
 }
