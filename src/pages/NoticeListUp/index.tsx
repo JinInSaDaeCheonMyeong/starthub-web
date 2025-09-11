a8e50729fe8a59e14461767ef4cd9d38c3980ddc
dlskl <skyandme77@gmail.com>
2025-09-11 21:35:07 +0900

    feat : add notice list up page #115

--- src/pages/NoticeListUp/index.tsx
+++ src/pages/NoticeListUp/index.tsx
@@ -1,9 +1,18 @@
 import FoldArrow from "@/shared/ui/FoldArrow";
 import Layout from "@/shared/ui/Layout";
 import { useParams } from "react-router-dom";
+import { useGetNotice } from "@/features/notice/getNotice/useGetNotice";
+import NoticeCard from "@/shared/ui/NoticeCard";
+import styled from "styled-components";
 
 const NoticeListUpPage = () => {
   const { type } = useParams();
+
+  const { data } = useGetNotice({
+    page: 0,
+    size: 20,
+    sort: [],
+  });
 
   return (
     <Layout>
@@ -12,9 +21,21 @@ const NoticeListUpPage = () => {
           type === "software" ? "IT/소프트웨어 분야 최신 공고" : "AI 추천 공고"
         }
       />
-      {/* notice 공고 띄우기 */}
+      <div style={{ marginBottom: "20px" }} />
+      <NoticeListContainer>
+        {data?.content.map((item) => (
+          <NoticeCard key={item.url} notice={item} />
+        ))}
+      </NoticeListContainer>
     </Layout>
   );
 };
 
 export default NoticeListUpPage;
+
+const NoticeListContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 20px;
+  margin-bottom: 20px;
+`;
