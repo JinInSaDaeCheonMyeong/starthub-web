12465ae6187071ecc9cc92c0fe818e5278fa584e
dlskl <skyandme77@gmail.com>
2025-09-15 17:16:12 +0900

    refactor : refactor notice page and dropdown #126

--- src/pages/Notice/index.tsx
+++ src/pages/Notice/index.tsx
@@ -1,49 +1,59 @@
 import Layout from "@/shared/ui/Layout";
 import SearchNotice from "@/features/notice/ui/searchNotice";
-import { NoticeData } from "@/entities/notice/model/notice.type.ts";
-import { useState, useCallback } from "react";
+import { useState } from "react";
 import NoticeCard from "@/shared/ui/NoticeCard";
 import * as S from "./style";
 import Pagination from "@/shared/ui/pagination";
+import { NoticeSearchParams } from "@/entities/notice/model/notice.type";
+import { useGetNoticeSearch } from "@/features/notice/getNoticeSearch/useGetNoticeSearch";
 
 const NoticePage = () => {
-  const [filteredNotices, setFilteredNotices] = useState<NoticeData[]>([]);
-  const [totalCount, setTotalCount] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
-  
-  const handleFilteredChange = useCallback(
-    (notices: NoticeData[], total: number) => {
-      setFilteredNotices(notices);
-      setTotalCount(total);
-    },
-    []
-  );
-  
+  const [filters, setFilters] = useState<
+    Omit<NoticeSearchParams, "page" | "size">
+  >({});
+
+  const { data, isLoading, isError } = useGetNoticeSearch({
+    ...filters,
+    page: currentPage - 1, // API가 0-based라면 이렇게
+    size: 16,
+    sort: "createdAt,desc",
+  });
+
+  const handleFilterChange = (
+    newFilters: Omit<NoticeSearchParams, "page" | "size">
+  ) => {
+    setFilters(newFilters);
+    setCurrentPage(1); // 필터 바뀌면 첫 페이지로 초기화
+  };
 
   return (
     <Layout>
-      <SearchNotice
-        onFilteredNoticesChange={handleFilteredChange}
-        currentPage={currentPage}
-      />
+      <SearchNotice onFilterChange={handleFilterChange} />
+
       <S.NoticeContentContainer>
-        {filteredNotices.length === 0 ? (
-          <div>
-            <p>검색 조건에 맞는 공고가 없습니다.</p>
-          </div>
+        {isLoading ? (
+          <p>로딩 중...</p>
+        ) : isError ? (
+          <p>데이터를 불러오는 중 오류가 발생했습니다.</p>
+        ) : !data || data.content.length === 0 ? (
+          <p>검색 조건에 맞는 공고가 없습니다.</p>
         ) : (
           <div style={{ display: "flex", flexWrap: "wrap" }}>
-            {filteredNotices.map((notice, index) => (
+            {data.content.map((notice, index) => (
               <NoticeCard key={`notice-${index}`} notice={notice} />
             ))}
           </div>
         )}
       </S.NoticeContentContainer>
-      <Pagination
-        currentPage={currentPage}
-        totalPages={Math.ceil(totalCount / 16)}
-        onPageChange={(page) => setCurrentPage(page)}
-      />
+
+      {data && data.totalPages > 1 && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={data.totalPages}
+          onPageChange={(page) => setCurrentPage(page)}
+        />
+      )}
     </Layout>
   );
 };
