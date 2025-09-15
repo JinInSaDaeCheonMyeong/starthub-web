99a45813a5d0133363f5a888f9f234ddfeb25112
dlskl <skyandme77@gmail.com>
2025-09-15 22:10:59 +0900

    fix : fix build error #134

--- src/entities/notice/api/notice.ts
+++ src/entities/notice/api/notice.ts
@@ -1,14 +1,14 @@
 import StartHubAxios from "@/shared/api/customAxios/StartHubAxios";
-import { NoticeResponse, NoticeParams, NoticePage, NoticeSearchParams } from "../model/notice.type";
+import { NoticeResponse, NoticeParams, NoticeSearchParams } from "../model/notice.type";
 
 export const NoticeApi = {
-  getNoticeAll: async (params: NoticeParams): Promise<NoticePage> => {
+  getNoticeAll: async (params: NoticeParams): Promise<NoticeResponse> => {
     const res = await StartHubAxios.get<NoticeResponse>("/announcements", {
       params,
     });
     return res.data;
   },
-  getNoticeSearch: async(params : NoticeSearchParams): Promise<NoticePage> => {
+  getNoticeSearch: async(params : NoticeSearchParams): Promise<NoticeResponse> => {
     const res = await StartHubAxios.get<NoticeResponse>("/announcements/search", {
       params,
     });
