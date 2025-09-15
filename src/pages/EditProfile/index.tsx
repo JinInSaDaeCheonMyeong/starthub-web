17a4a5c416e2435d5135a3f86671846b5922677f
yujunggg <a90134627@gmail.com>
2025-09-15 10:04:39 +0900

    feat: add EditMyProfile

---
+++ src/pages/EditProfile/index.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/widgets/Footer";
+import EditProfileForm from "@/features/profile/users/editProfileForm";
+import Header from "@/widgets/Header";
+
+const EditMyProfile = () => {
+  return (
+    <>
+      <Header />
+      <EditProfileForm />
+      <Footer />
+    </>
+  );
+};
+
+export default EditMyProfile;
