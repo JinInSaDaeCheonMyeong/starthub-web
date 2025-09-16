151d21c4665be7465be486695bd587995c0dab83
서진교 <oygnijoes0209@gmail.com>
2025-09-16 20:33:25 +0900

    fix: notice api

--- src/entities/notice/api/notice.ts
+++ src/entities/notice/api/notice.ts
@@ -1,18 +1,21 @@
 import StartHubAxios from "@/shared/api/customAxios/StartHubAxios";
-import { NoticeResponse, NoticeParams, NoticePage, NoticeSearchParams } from "../model/notice.type";
+import {
+  NoticeParams,
+  NoticePage,
+  NoticeSearchParams,
+} from "../model/notice.type";
 
 export const NoticeApi = {
   getNoticeAll: async (params: NoticeParams): Promise<NoticePage> => {
-    const res = await StartHubAxios.get<NoticeResponse>("/announcements", {
+    const res = await StartHubAxios.get("/announcements", {
       params,
     });
-    return res.data; 
+    return res.data;
   },
-  getNoticeSearch: async(params : NoticeSearchParams): Promise<NoticePage> => {
-    const res = await StartHubAxios.get<NoticeResponse>("/announcements/search", {
+  getNoticeSearch: async (params: NoticeSearchParams): Promise<NoticePage> => {
+    const res = await StartHubAxios.get("/announcements/search", {
       params,
     });
-    return res.data; 
+    return res.data;
   },
 };
-
