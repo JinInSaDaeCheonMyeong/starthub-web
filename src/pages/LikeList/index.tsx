328e50c08cb1b0ecc3d7ff00155ff3dbbf1f76ce
yujunggg <a90134627@gmail.com>
2025-09-17 14:46:27 +0900

    feat: create LikeList page

---
+++ src/pages/LikeList/index.tsx
@@ -0,0 +1,12 @@
+import LikeListForm from "@/features/profile/users/likeListForm"
+import Layout from "@/shared/ui/Layout";
+
+const LikeList = () => {
+  return (
+    <Layout>
+      <LikeListForm />
+    </Layout>
+  );
+};
+
+export default LikeList;
