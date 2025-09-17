b70e9bb9a93bd15a90b26bfe55b832bafadb9e64
dlskl <skyandme77@gmail.com>
2025-09-17 16:03:30 +0900

    feat : add notice detail page #130

---
+++ src/pages/NoticeDetail/index.tsx
@@ -0,0 +1,21 @@
+import { useParams } from "react-router-dom";
+import { useGetNoticeDetail } from "@/features/notice/getNoticeDetail/useGetNoticeDetail";
+import Layout from "@/shared/ui/Layout";
+import DetailContent from "./ui/DetailContent";
+import DetailSkeleton from "./ui/DetailSkeleton";
+
+const NoticeDetailPage = () => {
+  const { id } = useParams();
+  const noticeId = parseInt(id!, 10);
+
+  const { data, isLoading } = useGetNoticeDetail(noticeId, true);
+
+  return (
+    <Layout>
+      {isLoading ? <DetailSkeleton/> : 
+      data ? <DetailContent item={data} /> : ""}
+    </Layout>
+  );
+};
+
+export default NoticeDetailPage;
