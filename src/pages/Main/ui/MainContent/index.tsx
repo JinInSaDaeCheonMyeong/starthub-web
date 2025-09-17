34ada9857e0019775df44ccdfa13493c00fc7ab8
dlskl <skyandme77@gmail.com>
2025-09-17 16:08:45 +0900

    fix : fix section block path #130

--- src/pages/Main/ui/MainContent/index.tsx
+++ src/pages/Main/ui/MainContent/index.tsx
@@ -28,7 +28,7 @@ const MainContent = () => {
             ? "로그인 후 AI 맞춤형 공고를 추천받을 수 있어요!"
             : "AI 추천 공고"
         }
-        path="/sign-in"
+        path={isError ? "/sign-in" : "/notices/ai"}
       >
         <S.NoticeContainer>
           <RecommendedAINotice />
