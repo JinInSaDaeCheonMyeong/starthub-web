41fd716d3c9cb83845994d38633518fb5ea6140b
dlskl <skyandme77@gmail.com>
2025-09-17 15:48:42 +0900

    feat : add notice server connection #130

--- src/features/notice/getNoticeRecommend/useGetNoticeRecommend.ts
+++ src/features/notice/getNoticeRecommend/useGetNoticeRecommend.ts
@@ -4,14 +4,6 @@ import { NoticeApi } from "@/entities/notice/api/notice";
 import { NoticeType } from "@/entities/notice/model/notice.type";
 import { NOTICE_QUERY_KEYS } from "@/entities/notice/queryKey";
 
-const getToken = () => {
-  const cookies = document.cookie.split("; ");
-  const tokenCookie = cookies.find(
-    (cookie) => cookie.startsWith("access_token") || cookie.startsWith("token=")
-  );
-  return tokenCookie ? tokenCookie.split("=")[1] : null;
-};
-
 export const useGetNoticeRecommend = (
   options?: UseQueryOptions<NoticeType[], AxiosError, NoticeType[]>
 ) => {
@@ -19,9 +11,8 @@ export const useGetNoticeRecommend = (
     queryKey: NOTICE_QUERY_KEYS.notice.recommend(),
     queryFn: () => NoticeApi.getNoticeRecommend(),
     staleTime: 1000 * 60 * 5,
-    gcTime: 1000 * 60 * 10,
+    gcTime: 1000 * 60 * 10, 
     retry: false,
-    enabled: !!getToken(), 
     ...options,
   });
 };
