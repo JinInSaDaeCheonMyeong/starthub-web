e5b0d264ac23c521923961336fbb186844dc7267
yujunggg <a90134627@gmail.com>
2025-09-17 13:38:43 +0900

    chore: 버튼 페이지 연결

--- src/features/profile/users/sideBar/index.tsx
+++ src/features/profile/users/sideBar/index.tsx
@@ -1,21 +1,29 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import * as S from "./style";
 
 const MyPage: React.FC = () => {
   const [activeMenu, setActiveMenu] = useState<"프로필" | "좋아요">("프로필");
+  const navigate = useNavigate();
 
   return (
     <S.Sidebar>
       <S.SidebarItem
         className={activeMenu === "프로필" ? "active" : ""}
-        onClick={() => setActiveMenu("프로필")}
+        onClick={() => {
+          setActiveMenu("프로필");
+          navigate("/my-profile");
+        }}
       >
         프로필
       </S.SidebarItem>
 
       <S.SidebarItem
         className={activeMenu === "좋아요" ? "active" : ""}
-        onClick={() => setActiveMenu("좋아요")}
+        onClick={() => {
+          setActiveMenu("좋아요");
+          navigate("/like-list");
+        }}
       >
         좋아요
       </S.SidebarItem>
