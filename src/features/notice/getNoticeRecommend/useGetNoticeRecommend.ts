fb05da484fb9ed9c7f1d48c3a8c563fc38300b7b
dlskl <skyandme77@gmail.com>
2025-09-17 15:48:42 +0900

    feat : add notice server connection #130

---
+++ src/features/notice/getNoticeRecommend/useGetNoticeRecommend.ts
@@ -0,0 +1,18 @@
+import { useQuery, UseQueryOptions } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { NoticeApi } from "@/entities/notice/api/notice";
+import { NoticeType } from "@/entities/notice/model/notice.type";
+import { NOTICE_QUERY_KEYS } from "@/entities/notice/queryKey";
+
+export const useGetNoticeRecommend = (
+  options?: UseQueryOptions<NoticeType[], AxiosError, NoticeType[]>
+) => {
+  return useQuery({
+    queryKey: NOTICE_QUERY_KEYS.notice.recommend(),
+    queryFn: () => NoticeApi.getNoticeRecommend(),
+    staleTime: 1000 * 60 * 5,
+    gcTime: 1000 * 60 * 10, 
+    retry: false,
+    ...options,
+  });
+};
