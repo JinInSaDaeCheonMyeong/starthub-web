a2bccfc49b46df35cad6beee0972834910996997
yujunggg <a90134627@gmail.com>
2025-09-17 14:47:24 +0900

    chore: add LikeList router

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -45,6 +45,7 @@ const Router = () => {
         <Route path="/notices/:type" element={<NoticeListUpPage/>}/>
         <Route path="/like-list" element={<LikeList/>}/>
         <Route path="/notice/:id" element={<NoticeDetailPage/>}/>
+        <Route path="/like-list" element={<LikeList/>}/>
       </Routes>
     </BrowserRouter>
   )
