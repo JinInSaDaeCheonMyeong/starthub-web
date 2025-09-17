bbf213ed6c812c12bda456c1db4161c154f81090
yujunggg <a90134627@gmail.com>
2025-09-17 14:44:45 +0900

    chore: add notice queryKey

--- src/entities/notice/queryKey.ts
+++ src/entities/notice/queryKey.ts
@@ -4,5 +4,6 @@ export const NOTICE_QUERY_KEYS = {
   notice: {
     all: (params: NoticeParams) => ["notice", "all", params] as const,
     search: (params : NoticeSearchParams) => ["notice", "search", params] as const,
+    likes: (params: NoticeParams) => ["notice", "likes", params] as const,
   },
 };
