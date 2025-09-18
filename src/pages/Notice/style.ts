ce38827b0ded95a8248a872ddb986e6a5e0380fa
dlskl <skyandme77@gmail.com>
2025-09-18 11:27:43 +0900

    fix : fix style no notice #147

--- src/pages/Notice/style.ts
+++ src/pages/Notice/style.ts
@@ -1,7 +1,21 @@
+import { StartHubFont } from "@/shared/design";
 import styled from "styled-components";
 
 export const NoticeContentContainer = styled.div`
   margin-top: 50px;
   width: 100%;
   padding: 0 200px;
+  min-height: 50vh;
+`;
+
+export const CardWrap = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const ExceptionMessage = styled.p`
+  ${StartHubFont.Pretendard.Body1.Medium}
+  display: flex;
+  justify-content: center;
+  margin-top: 60px;
 `;
