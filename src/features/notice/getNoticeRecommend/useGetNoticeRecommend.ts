d2666863749520475958e9cc9fba265bd36e3a80
dlskl <skyandme77@gmail.com>
2025-09-18 19:32:59 +0900

    refactor : turn getToken into logIn #151

--- src/features/notice/getNoticeRecommend/useGetNoticeRecommend.ts
+++ src/features/notice/getNoticeRecommend/useGetNoticeRecommend.ts
@@ -3,25 +3,20 @@ import { AxiosError } from "axios";
 import { NoticeApi } from "@/entities/notice/api/notice";
 import { NoticeType } from "@/entities/notice/model/notice.type";
 import { NOTICE_QUERY_KEYS } from "@/entities/notice/queryKey";
-
-const getToken = () => {
-  const cookies = document.cookie.split("; ");
-  const tokenCookie = cookies.find(
-    (cookie) => cookie.startsWith("access_token") || cookie.startsWith("token=")
-  );
-  return tokenCookie ? tokenCookie.split("=")[1] : null;
-};
+import { useAuthStore } from "@/app/model/stores/useAuthStore";
 
 export const useGetNoticeRecommend = (
   options?: UseQueryOptions<NoticeType[], AxiosError, NoticeType[]>
 ) => {
+  const { isLoggedIn } = useAuthStore();
+
   return useQuery({
     queryKey: NOTICE_QUERY_KEYS.notice.recommend(),
     queryFn: () => NoticeApi.getNoticeRecommend(),
     staleTime: 1000 * 60 * 5,
     gcTime: 1000 * 60 * 10,
     retry: false,
-    enabled: !!getToken(), 
+    enabled: isLoggedIn,
     ...options,
   });
 };
