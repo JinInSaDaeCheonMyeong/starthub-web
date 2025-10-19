ff8a43875437c09e2ac6e8df2c0e98fc5322f575
yujunggg <a90134627@gmail.com>
2025-10-19 23:12:10 +0900

    feat: integrate competitor analysis pages and routing

---
+++ src/pages/Competitor/List/index.tsx
@@ -0,0 +1,12 @@
+import CompetitorTemplateCard from "@/shared/ui/CompetitorTemplateCard";
+import Layout from "@/shared/ui/Layout";
+
+const CompetitorList = () => {
+  return (
+    <Layout>
+      <CompetitorTemplateCard />
+    </Layout>
+  );
+};
+
+export default CompetitorList;
