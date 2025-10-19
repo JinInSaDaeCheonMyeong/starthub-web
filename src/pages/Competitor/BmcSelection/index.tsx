d0cb605efc495bf88ec7cf5862e70c4d25dc7008
yujunggg <a90134627@gmail.com>
2025-10-19 23:12:10 +0900

    feat: integrate competitor analysis pages and routing

---
+++ src/pages/Competitor/BmcSelection/index.tsx
@@ -0,0 +1,12 @@
+import BmcList from "@/shared/ui/BmcSelection";
+import Layout from "@/shared/ui/Layout";
+
+const CompetitorBmcSelection = () => {
+  return (
+    <Layout>
+      <BmcList />
+    </Layout>
+  );
+};
+
+export default CompetitorBmcSelection;
