f2dbaacbc6cab5ca890fd76f1de7f3fa3dfe74f1
yujunggg <a90134627@gmail.com>
2025-10-19 23:12:10 +0900

    feat: integrate competitor analysis pages and routing

--- src/pages/Competitor/List/index.tsx
+++ src/pages/Competitor/List/index.tsx
@@ -1,31 +1,12 @@
 import CompetitorTemplateCard from "@/shared/ui/CompetitorTemplateCard";
 import Layout from "@/shared/ui/Layout";
-import NotMyPage from "@/features/profile/users/profileForm/ui/NotMyPage";
-import { useAuthStore } from "@/app/model/stores/useAuthStore";
-import styled from "styled-components";
-
 
 const CompetitorList = () => {
-  const {isLoggedIn} = useAuthStore()
-
   return (
     <Layout>
-      {isLoggedIn ? (
-        <CompetitorTemplateCard />
-      ) : (
-        <NotMyPageWrap>
-          <NotMyPage />
-        </NotMyPageWrap>
-      )}
+      <CompetitorTemplateCard />
     </Layout>
   );
 };
 
 export default CompetitorList;
-
-
-const NotMyPageWrap = styled.div`
-  min-height: 60vh;
-  display: flex;
-  justify-content: center;
-`
