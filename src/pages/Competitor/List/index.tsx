d072c98cdd065c00de68d53c0a4c9b30d77de41e
dlskl <skyandme77@gmail.com>
2025-10-20 23:35:33 +0900

    fix: handle when no user exists #171

--- src/pages/Competitor/List/index.tsx
+++ src/pages/Competitor/List/index.tsx
@@ -1,10 +1,21 @@
 import CompetitorTemplateCard from "@/shared/ui/CompetitorTemplateCard";
 import Layout from "@/shared/ui/Layout";
+import NotMyPage from "@/features/profile/users/profileForm/ui/NotMyPage";
+import { useAuthStore } from "@/app/model/stores/useAuthStore";
+
 
 const CompetitorList = () => {
+  const {isLoggedIn} = useAuthStore()
+
   return (
     <Layout>
-      <CompetitorTemplateCard />
+      {isLoggedIn ? (
+        <CompetitorTemplateCard />
+      ) : (
+        <div style={{minHeight:"60vh", display:"flex", justifyContent:"center"}}>
+          <NotMyPage />
+        </div>
+      )}
     </Layout>
   );
 };
