5e458fca85ba4a64fbaa466fb584a1213827273e
서진교 <oygnijoes0209@gmail.com>
2025-10-20 03:33:03 +0100

    feat: add BMC session modal #52

--- src/features/bmc/SessionModal/style.ts
+++ src/features/bmc/SessionModal/style.ts
@@ -8,7 +8,7 @@ export const Overlay = styled.div`
   left: 0;
   right: 0;
   bottom: 0;
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: ${StartHubColors.Black1};
   display: flex;
   align-items: center;
   justify-content: center;
