0b82914ed01daca47ec05ae17b394a8594d0c5e5
dlskl <skyandme77@gmail.com>
2025-10-20 23:35:33 +0900

    fix: handle when no user exists #171

--- src/pages/Bmc/BmcPage/index.tsx
+++ src/pages/Bmc/BmcPage/index.tsx
@@ -1,12 +1,28 @@
 import BmcList from "@/shared/ui/BmcList";
 import BmcTemplateCard from "@/entities/bmc/ui/BmcTemplateCard";
-import Layout from '@/shared/ui/Layout';
-
+import Layout from "@/shared/ui/Layout";
+import { useAuthStore } from "@/app/model/stores/useAuthStore";
+import NotMyPage from "@/features/profile/users/profileForm/ui/NotMyPage";
 const BmcPage = () => {
+  const { isLoggedIn } = useAuthStore();
   return (
     <Layout>
-      <BmcTemplateCard/>
-      <BmcList/>
+      {isLoggedIn ? (
+        <>
+          <BmcTemplateCard />
+          <BmcList />
+        </>
+      ) : (
+        <div
+          style={{
+            minHeight: "60vh",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <NotMyPage />
+        </div>
+      )}
     </Layout>
   );
 };
