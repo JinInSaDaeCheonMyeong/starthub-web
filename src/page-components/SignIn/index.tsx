69ecfeacb2934159a97d2d287bb8f1b705ee8122
서진교 <oygnijoes0209@gmail.com>
2026-05-05 03:10:21 +0900

    feat: 로그인 페이지 Figma 디자인 적용 및 반응형 구현
    - 로그인 페이지 레이아웃을 Figma 디자인에 맞게 수정
    - 모바일에서 일러스트레이션 숨김, 데스크톱에서 표시하는 반응형 구현
    - 소셜 로그인 버튼 스타일링 개선 (Google 버튼 테두리 수정)
    - 로그인 폼을 proper form 요소로 감싸고 submit 처리 추가
    - 이메일, 비밀번호 필드에 적절한 autocomplete 속성 추가

--- src/page-components/SignIn/index.tsx
+++ src/page-components/SignIn/index.tsx
@@ -3,12 +3,16 @@ import { ReactComponent as SignInImage } from "@assets/images/signInImage.svg";
 
 const SignInPage = () => {
   return (
-    <div className="flex h-screen">
-      <div className="w-1/2 bg-hub-primary flex justify-center items-center">
-        <SignInImage className="w-full h-full" />
+    <div className="flex min-h-screen bg-white">
+      {/* Left Section - Illustration */}
+      <div className="hidden lg:flex lg:w-1/2 bg-hub-primary items-center justify-center relative overflow-hidden">
+        <div className="relative w-full max-w-[640px] h-[724px]">
+          <SignInImage className="w-full h-full" />
+        </div>
       </div>
 
-      <div className="w-1/2 flex relative justify-center items-center">
+      {/* Right Section - Sign In Form */}
+      <div className="w-full lg:w-1/2 flex items-center justify-center px-4 sm:px-6 lg:px-8">
         <SignInBox />
       </div>
     </div>
