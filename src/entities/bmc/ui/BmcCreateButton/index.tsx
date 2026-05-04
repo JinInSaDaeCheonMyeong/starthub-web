46d5f36aa13dab8a73c3d5012586c58a80d515f5
dlskl <skyandme77@gmail.com>
2026-05-04 10:21:41 +0900

    refactor: BMC 엔터티 색상을 CSS 변수로 업데이트

--- src/entities/bmc/ui/BmcCreateButton/index.tsx
+++ src/entities/bmc/ui/BmcCreateButton/index.tsx
@@ -1,14 +1,22 @@
 import { ReactComponent as Plus } from "@assets/icons/plus.svg";
-import * as S from './style'
 
 const BmcCreateButton = () => {
   return (
-    <S.ButtonContainer onClick={()=>{}}>
-      <S.PlusIcon>
+    // ButtonContainer
+    <div
+      onClick={() => {}}
+      className="
+        w-[282px] h-[173px] border border-hub-gray-3 rounded-[10px]
+        flex items-center justify-center cursor-pointer transition-all duration-300
+        hover:border-hub-primary hover:bg-hub-white-2
+      "
+    >
+      {/* PlusIcon */}
+      <div className="w-[60px] h-[60px] flex items-center justify-center [&_svg]:w-full [&_svg]:h-full [&_svg]:fill-hub-primary">
         <Plus />
-      </S.PlusIcon>
-    </S.ButtonContainer>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default BmcCreateButton
+export default BmcCreateButton;
--- src/entities/bmc/ui/BmcTemplateCard/index.tsx
+++ src/entities/bmc/ui/BmcTemplateCard/index.tsx
@@ -2,55 +2,65 @@ import { ReactComponent as StartHubTemplate } from "@assets/images/templates/sta
 import { ReactComponent as StartHubDarkTemplate } from "@assets/images/templates/darkBmc.svg";
 import { ReactComponent as SimpleTemplate } from "@/assets/images/templates/simpleBmc.svg";
 import { ReactComponent as ColorTemplate } from "@/assets/images/templates/colorBmc.svg";
-import * as S from "./style";
 import { useState } from "react";
 import BusinessTemplateModal from "@/features/bmc/SessionModal";
 import Portal from "@/shared/ui/Portal";
 import { BmcTemplateType } from "@/entities/bmc/model/types";
 
+const templates: {
+  component: React.FC<React.SVGProps<SVGSVGElement>>;
+  type: BmcTemplateType;
+  title: string;
+}[] = [
+  { component: StartHubTemplate, type: "STARTHUB", title: "StartHub" },
+  {
+    component: StartHubDarkTemplate,
+    type: "STARTHUB_DARK",
+    title: "StartHub Dark",
+  },
+  { component: SimpleTemplate, type: "SIMPLE", title: "Simple" },
+  { component: ColorTemplate, type: "COLOR", title: "Color" },
+];
+
 const BmcTemplateCard = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedTemplate, setSelectedTemplate] = useState<BmcTemplateType>("STARTHUB");
+  const [selectedTemplate, setSelectedTemplate] =
+    useState<BmcTemplateType>("STARTHUB");
 
   const openModal = (templateType: BmcTemplateType) => {
     setSelectedTemplate(templateType);
     setIsModalOpen(true);
   };
 
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleCreateSession = () => {
-    closeModal();
-  };
+  const closeModal = () => setIsModalOpen(false);
 
   return (
     <>
-      <S.Container>
-        <S.Text>BMC를 작성해보세요!</S.Text>
-        <S.BmcTemplateContainer>
-          <S.ImageWrapper>
-            <StartHubTemplate onClick={() => openModal("STARTHUB")} />
-            <S.Title>StartHub</S.Title>
-          </S.ImageWrapper>
+      {/* Container */}
+      <div className="w-[1024px] mt-10">
+        {/* Text */}
+        <p className="font-pt-h2-semibold mb-5">BMC를 작성해보세요!</p>
 
-          <S.ImageWrapper>
-            <StartHubDarkTemplate onClick={() => openModal("STARTHUB_DARK")} />
-            <S.Title>StartHub Dark</S.Title>
-          </S.ImageWrapper>
-
-          <S.ImageWrapper>
-            <SimpleTemplate onClick={() => openModal("SIMPLE")} />
-            <S.Title>Simple</S.Title>
-          </S.ImageWrapper>
-
-          <S.ImageWrapper>
-            <ColorTemplate onClick={() => openModal("COLOR")} />
-            <S.Title>Color</S.Title>
-          </S.ImageWrapper>
-        </S.BmcTemplateContainer>
-      </S.Container>
+        {/* BmcTemplateContainer */}
+        <div className="flex justify-between w-[1024px]">
+          {templates.map(({ component: Svg, type, title }) => (
+            // ImageWrapper
+            <div key={type} className="w-[242px] bg-hub-white-1">
+              <Svg
+                onClick={() => openModal(type)}
+                className="
+                  w-[242px] h-[170px] rounded-[10px] border border-hub-gray-3
+                  mb-0.5 cursor-pointer transition-opacity duration-300
+                  hover:opacity-50
+                "
+              />
+              <p className="text-hub-black-1 font-pt-caption1-regular">
+                {title}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
 
       {isModalOpen && (
         <Portal>
@@ -58,7 +68,7 @@ const BmcTemplateCard = () => {
             isOpen={isModalOpen}
             templateType={selectedTemplate}
             onClose={closeModal}
-            onGenerateBmc={handleCreateSession}
+            onGenerateBmc={closeModal}
           />
         </Portal>
       )}
