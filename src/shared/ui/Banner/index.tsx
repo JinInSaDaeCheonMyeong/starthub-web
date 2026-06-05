ab77e9e48ad34a3a0bd893b2516ea2a421acc900
dlskl <skyandme77@gmail.com>
2026-06-05 12:38:43 +0900

    refactor: optimize banner images #246

--- src/shared/ui/Banner/index.tsx
+++ src/shared/ui/Banner/index.tsx
@@ -1,22 +1,24 @@
-import bannerImage from "@/assets/images/image.png";
+import Image from "next/image";
+import bannerImage from "@/assets/images/image.webp";
 import mobileBanner from "@/assets/images/mobile-banner.png";
 
 const Banner = () => {
   return (
     <div className="mt-[92px] md:mt-[110px] w-screen -ml-[calc((100vw-100%)/2)]">
       <div className="md:hidden w-full h-[200px]">
-        <img
-          src={mobileBanner.src}
+        <Image
+          src={mobileBanner}
           alt="StartHub 모바일 배너"
           className="w-full h-full object-fill"
+          priority
         />
       </div>
-
       <div className="hidden md:block w-full h-[274px]">
-        <img
-          src={bannerImage.src}
+        <Image
+          src={bannerImage}
           alt="StartHub 배너"
           className="w-full h-full object-cover"
+          priority
         />
       </div>
     </div>
